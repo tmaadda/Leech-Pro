0e557e0e44ae50700b4f7054d421c2c3d811710c
tmaadda <96387287+tmaadda@users.noreply.github.com>
2022-05-12 16:15:34 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -61,9 +61,9 @@ async def upload_to_tg(
 ):
     base_file_name = os.path.basename(local_file_name)
     caption_str = ""
-    caption_str += "<code>"
+    caption_str += "<b>"
     caption_str += base_file_name
-    caption_str += "</code>"
+    caption_str += "</b>"
     if os.path.isdir(local_file_name):
         directory_contents = os.listdir(local_file_name)
         directory_contents.sort()
@@ -104,7 +104,7 @@ async def upload_to_tg(
             ba_se_file_name = os.path.basename(local_file_name)
             await i_m_s_g.edit_text(
                 f"Detected File Size: {d_f_s} 😡\n"
-                f"<code>{ba_se_file_name}</code> splitted into {number_of_files} files.\n"
+                f"<b>{ba_se_file_name}</b> splitted into {number_of_files} files.\n"
                 "Trying to upload to Telegram, now ..."
             )
             for le_file in totlaa_sleif:
